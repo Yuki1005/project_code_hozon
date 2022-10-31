8cea9cb82d198fa9fc1646e00fbc529c7b4afcd0
Yuki1005 <yhiramatsu5@icloud.com>
2022-10-31 12:06:27 +0900

    a

--- project_code_final.py
+++ project_code_final.py
@@ -14,7 +14,7 @@ wait_time_heikin = 0
 wait_time_takeoff = 0
 wait_time_arrive = 0
 
-kankaku = 60
+kankaku = int(input("何分、間隔を設けるか"))*60
 
 G = nx.read_weighted_edgelist("dijkstra_data.txt", create_using=nx.DiGraph)
 pattern1 = pd.read_csv("project_code_final1_data.csv")
